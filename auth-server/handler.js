3534422167fa6f2da0f40ccabad706ddf41ee74a
DaniLott1988 <danilott1988@gmail.com>
2022-02-07 22:25:01 +0100

    Task 4.2.7

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -35,7 +35,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
       authUrl: authUrl,
@@ -64,7 +64,7 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify(token),
       };
@@ -74,7 +74,7 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 500,
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify(err),
       };
@@ -114,7 +114,7 @@ module.exports.getCalendarEvents = async (event) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({ events: results.data.items }),
     };
@@ -124,7 +124,7 @@ module.exports.getCalendarEvents = async (event) => {
     return {
       statusCode: 500,
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify(err),
     };
