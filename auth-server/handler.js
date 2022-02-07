50df0dd43f92053e1c72dab2d2c63c25c6686abb
DaniLott1988 <danilott1988@gmail.com>
2022-02-07 12:50:30 +0100

    Task 4.2.2

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://DaniLott1988.github.io/a4meet/"],
+  redirect_uris: ["https://danilott1988.github.io/a4meet/"],
   javascript_origins: ["https://DaniLott1988.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
