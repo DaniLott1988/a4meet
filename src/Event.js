13a6352e2b09ed8e4a6e0f87ddd88d77897ae898
DaniLott1988 <danilott1988@gmail.com>
2022-02-10 15:01:39 +0100

    Task 4.4.6

--- src/Event.js
+++ src/Event.js
@@ -16,23 +16,25 @@ class Event extends Component {
     const { event} = this.props;
     const { collapsed } = this.state;
     
-    return <div className='event'>
+    return (
+      <div className='event'>
       
-      <p className='summary'>{event.summary}</p>
-      <p className='dateTime'>{event.dateTime}</p>
-      <p className='timeZone'>{event.timeZone}</p>
-      <p className='location'>@{event.summary} | {event.location}</p>
+        <p className='summary'>{event.summary}</p>
+        <p className='dateTime'>{event.dateTime}</p>
+        <p className='timeZone'>{event.timeZone}</p>
+        <p className='location'>@{event.summary} | {event.location}</p>
 
-      {!collapsed && (
-        <div className="details-view">
-          <h2 className="details-header">About event:</h2>
-            <a href={event.htmlLink} className='htmlLink' rel="noreferrer" target='_blank'>See details on Google Calendar</a>
-              <p className='description'>{event.description}</p>
-        </div>
-        )}
-        <button className={`${collapsed ? "show" : "hide"}-details`}
-        onClick={this.handleClick}>{collapsed ? "Show Details" : "Hide-Details"}</button>
-    </div>  
+        {!collapsed && (
+          <div className="details-view">
+            <h2 className="details-header">About event:</h2>
+              <a href={event.htmlLink} className='htmlLink' rel="noreferrer" target='_blank'>See details on Google Calendar</a>
+                <p className='description'>{event.description}</p>
+          </div>
+          )}
+          <button className={`${collapsed ? "show" : "hide"}-details`}
+          onClick={this.handleClick}>{collapsed ? "Show Details" : "Hide-Details"}</button>
+      </div>
+    )  
   }
 }
 
