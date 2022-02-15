81efe829287b2cfa45fca483398dc8dc7053be29
DaniLott1988 <danilott1988@gmail.com>
2022-02-15 16:20:13 +0100

    Task 4.7.3

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,17 +5,20 @@ class NumberOfEvents extends Component {
   
   state = {
     numberOfEvents: '32',
+    infoText: "",
   }
   
   handleInputChanged = (event) => {
     const number = event.target.value;
     if (number <= 0 || number > 32) {
       this.setState({
-        infoText: 'Please input a number between 1 and 32.'
+        numberOfEvents: "",
+        infoText: "Please enter a number between 1 and 32.",
       });
     } else {
       this.setState({
         numberOfEvents: number,
+        infoText: "",
       });
       this.props.updateNumberofEvents(number);
     }
