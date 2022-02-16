05746d0f224fb85ed74335f4a5068c2336edf0de
DaniLott1988 <danilott1988@gmail.com>
2022-02-16 15:59:03 +0100

    Task 4.8.1

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -43,6 +43,7 @@ class CitySearch extends Component {
     return (
       <div className='CitySearch'>
         <InfoAlert text={this.state.infoText} />
+        <p>Choose your city:</p>
         <input
           type="text"
           className="city"
