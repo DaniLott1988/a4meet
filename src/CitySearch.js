5d36913d99dd4e8797b753b79e253c0479ef2db8
DaniLott1988 <danilott1988@gmail.com>
2022-02-17 19:37:36 +0100

    Task 4.9.3

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -11,7 +11,6 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({showSuggestions:true});
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
@@ -19,13 +18,15 @@ class CitySearch extends Component {
       this.setState({
         query: value,
         infoText: 'We can not find the city you are looking for. Please try another city',
+        showSuggestions: false
       });
     } else {
       return this.setState({
         query: value,
         suggestions,
         showSuggestions:false,
-        infoText:''
+        infoText:'',
+        showSuggestions: true
       });
     }
   };
